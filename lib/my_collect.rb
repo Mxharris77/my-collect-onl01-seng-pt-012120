bb3cd5498d03d6442bfc58a829498f614a941f8f
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 19:47:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
- def my_collect(empty_array)  do |x|
-   counter += 1
-   
- end 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
  
   
